6ad2f088c6aad0e41c133217a69724f8c6c56fdc
whoami-anoint <abhishekkfl@gmail.com>
2023-08-21 19:57:07 +0530

    update

--- build.sh
+++ build.sh
@@ -1,33 +1,34 @@
 #!/bin/bash
 
-# Stop and disable any previously running services
+# Step 1: Stop and disable any previously running services
 sudo systemctl stop nginx
 sudo systemctl stop gunicorn
 
-# Update your codebase (assumes you're using git)
+# Step 2: Update your codebase (assumes you're using git)
 git pull origin master
 
-# Activate virtual environment
+# Step 3: Activate virtual environment
 source venv/bin/activate
 
-# Install or update dependencies
+# Step 4: Install or update dependencies
 pip install -r requirements.txt
 
-# Collect static files
-python manage.py collectstatic --noinput
-
-# Apply database migrations
-python manage.py migrate
-
-# Restart services
-sudo systemctl start gunicorn
-sudo systemctl start nginx
-
-# Reload Nginx configuration
-sudo systemctl reload nginx
-
-# Install and initialize Tailwind CSS
+# Step 5: Install and initialize Tailwind CSS
 npm install -D tailwindcss
 npx tailwindcss init
 
+# Step 6: Collect static files
+python manage.py collectstatic --noinput
+
+# Step 7: Apply database migrations
+python manage.py migrate
+
+# Step 8: Restart services
+sudo systemctl start gunicorn
+sudo systemctl start nginx
+
+# Step 9: Reload Nginx configuration
+sudo systemctl reload nginx
+
+# Step 10: Display completion message
 echo "Deployment complete!"
