a4fbd58d0eacc5db0d746bd0acb411087b35c5e2
whoami-anoint <abhishekkfl@gmail.com>
2023-08-21 19:27:44 +0530

    update

---
+++ build.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Stop and disable any previously running services
+sudo systemctl stop nginx
+sudo systemctl stop gunicorn
+
+# Update your codebase (assumes you're using git)
+git pull origin master
+
+# Activate virtual environment
+source venv/bin/activate     
+
+# Install or update dependencies
+pip install -r requirements.txt
+
+# Collect static files
+python manage.py collectstatic --noinput
+
+# Apply database migrations
+python manage.py migrate
+
+# Restart services
+sudo systemctl start gunicorn
+sudo systemctl start nginx
+
+# Reload Nginx configuration
+sudo systemctl reload nginx
+
+echo "Deployment complete!"
